6dc0406fb966774ded894348bbcb819062437ccc
Juan Barrera <juanbarrera8902@gmail.com>
2024-01-11 19:45:55 +0100

    ADD ClientTestApp style file

--- CLIENTVIEW/main.py
+++ CLIENTVIEW/main.py
@@ -2,13 +2,11 @@
 #python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
 #python -m pip install kivy.deps.gstreamer
 #pip install kivy
-
 from kivy.app import App
-from kivy.uix.label import Label
 
 class Alicia(App):
     def build(self):
-        return Label(text="Hola Mundo")
+        self.title = 'Alicia v.0.0.1'
 
 
 if __name__ == '__main__':
