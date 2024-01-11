94a8718ecc5032a05bf69732202d808bb88d3588
Juan Barrera <juanbarrera8902@gmail.com>
2024-01-11 17:30:24 +0100

    Update main.py

--- CLIENTVIEW/main.py
+++ CLIENTVIEW/main.py
@@ -1,15 +1,15 @@
 #python -m pip install --upgrade pip wheel setuptools
 #python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
 #python -m pip install kivy.deps.gstreamer
-#python –m pip install kivy
+#pip install kivy
 
 from kivy.app import App
 from kivy.uix.label import Label
 
-class KivyTest(App):
+class Alicia(App):
     def build(self):
         return Label(text="Hola Mundo")
 
 
 if __name__ == '__main__':
-    KivyTest().run()
+    Alicia().run()
