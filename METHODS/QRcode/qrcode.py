cbaa849f3c9b39ffd4a0494700f070746b96b978
Andrew <andrewapn38@gmail.com>
2024-02-15 11:37:10 +0100

    Update QRcode Method

--- METHODS/QRcode/qrcode.py
+++ METHODS/QRcode/qrcode.py
@@ -1,22 +1,37 @@
 # Importar librerias
 import cv2
-import numpy as np
+import webbrowser
 
-capture = cv2. VideoCapture (0)
+# Función para abrir una página web en el navegador
+def abrir_pagina_web(url):
+    webbrowser.open(url)
 
-while(capture.isOpened()):
-    ret, frame = capture. read ()
+# Inicializarmos la captura de la cámara
+capture = cv2.VideoCapture(0)
+
+# Bucle principal para capturar imágenes de la cámara
+while capture.isOpened():
+    # Leemos un nuevo fotograma de la cámara
+    ret, frame = capture.read()
     
-    if (cv2.waitKey(1) == ord('s')):
-        break
-    qrDetector = cv2. QRCodeDetector()
+    # Detectamos y decodificamos si hay un posible código QR en el fotograma
+    qrDetector = cv2.QRCodeDetector()
     data, bbox, rectifiedImage = qrDetector.detectAndDecode(frame)
     
+    # Si se detecta un código QR
     if len(data) > 0:
         print(f'Dato: {data}')
+        # Abrir la página web correspondiente al dato del código QR
+        abrir_pagina_web(data)
         cv2.imshow('webCam', rectifiedImage)
     else: 
+        # Si no se detecta un código QR, mostrar el fotograma original
         cv2.imshow('webCam', frame) 
     
-    capture.release()
-    cv2.destroyAllWindows
+    # Esperar hasta que se presione la tecla 'Esc' para salir del bucle
+    if cv2.waitKey(1) == 27:  # 27 corresponde al código ASCII de la tecla 'Esc'
+        break
+
+# Liberamos la captura de la cámara y cerrar todas las ventanas
+capture.release()
+cv2.destroyAllWindows()
