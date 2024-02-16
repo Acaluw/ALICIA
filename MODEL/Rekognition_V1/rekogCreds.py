cf0690606d9515a62f56f1b947ba0ea783acdba0
Juan Barrera <juanbarrera8902@gmail.com>
2024-02-16 18:37:25 +0100

    MOD clean old api keys

--- MODEL/Rekognition_V1/rekogCreds.py
+++ MODEL/Rekognition_V1/rekogCreds.py
@@ -7,6 +7,6 @@ region = 'us-east-1'
 bucket = 'aliciabucketv1'
 
 # Credenciales de AWS
-aws_access_key_id = 'ASIA2FISX6YKH4I6X7E4'
-aws_secret_access_key = 'RZS2M1TUgefbXF3CiNV3Rs4o4K+3wXu7p2Y6hPdp'
-aws_session_token = 'FwoGZXIvYXdzEM7//////////wEaDC/7SuxjCkt0fPqRNiLGAWsYqxq+FyVgYyQUFE+HqJwJsXPWC2oS58AeMvt7c9A93peyqfeaaxqfVItHiWWhF9r+AIU3I/84F4+MekxrHzBjpDE1suuKrcPq5FFftih3qxKM/K9dFZrUdisejJc2RjjTfzbESGJVB9oTwcAbVQ9mUXz3zyjPHJdgP3lwonqRs9I8Yf6C4BNYjhkW05jL/acJCn/pJpJy0lW89/H0xE4gyriGT1nFqZmw8EKLAvFar8IS0q2c6ccyXyNi+bI15huRzwgDpiiLwrSuBjIt/RgUJuK29+Cfg5jKE70BEGVJwz0UPArbpoqwwcAjSu0Zbqrk1Qxx3BoDBE5n'
+aws_access_key_id = 'API_KEY_HERE'
+aws_secret_access_key = 'API_KEY_HERE'
+aws_session_token = 'API_KEY_HERE'
