d2811aad560e4858b5b4dc425d9888ffa6c3eee9
Juan Barrera <juanbarrera8902@gmail.com>
2024-02-18 14:50:05 +0100

    MOD  New GUI and linked Threads

--- MODEL/SpeechToText_V1/main.py
+++ MODEL/SpeechToText_V1/main.py
@@ -2,12 +2,10 @@
 
 # Libs import
 import sys
+import time
+from threading import Event
 from pathlib import Path
 import speech_recognition as sr
-import threading
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s' , filename="TEMPFILES/mainLog.log",filemode="a")
 
 # Making TextToSpeech/main.py visible for this file
 root_path = Path(__file__).resolve().parents[1]
@@ -17,20 +15,19 @@ from TextToSpeech_V1 import main as tts
 # Main Variables
 voiceRecog = sr.Recognizer()
 activeBool = False
-activeModel = threading.Event()
+camCapture = False
+camCaptureChanged = Event()
 
 # Method that listen for a keyword to activate listen() method
 def listenKeyWord():
     with sr.Microphone() as source:
         print("SpeechToText || Waiting for keyword")
-        logging.info('SpeechToText || Waiting for keyword')
         voiceRecog.adjust_for_ambient_noise(source, duration=1)
         audio = voiceRecog.listen(source)
 
     try:
         texto = voiceRecog.recognize_google(audio, language='es')
         print(f'SpeechToText || Keyword: {texto}')
-        logging.info(f'SpeechToText || Keyword: {texto}')
         if 'Alicia' in texto:
             return True
     except sr.UnknownValueError:
@@ -40,14 +37,12 @@ def listenKeyWord():
 def listen():
     with sr.Microphone() as source:
         print("SpeechToText || Command section...")
-        logging.info('SpeechToText || Command section...')
         voiceRecog.adjust_for_ambient_noise(source, duration=1)
         audio = voiceRecog.listen(source)
 
     try:
         texto = voiceRecog.recognize_google(audio,language='es')
         print("SpeechToText || Command detected...")
-        logging.info('SpeechToText || Command detected...')
         return texto.lower()
     except sr.UnknownValueError:
         return ""
@@ -55,6 +50,7 @@ def listen():
 # Active loop that keep listening for the keyword
 def runSpeechModel():
     global activeBool
+    global camCapture
     while True:
         if not activeBool:
             if listenKeyWord():
@@ -63,7 +59,6 @@ def runSpeechModel():
         else:
             action = listen()
             print(f'SpeechToText || Action: {action}')
-            logging.info(f'SpeechToText || Action: {action}')
             if 'prueba de sonido' in action: # Performs a sound test
                 activeBool = tts.soundTest()
             elif 'hora es' in action: # Get the actual time of the user
@@ -80,17 +75,20 @@ def runSpeechModel():
                 elif 'pon' in action:
                     songName = action.split('reproduce')[1].strip()
                 activeBool = tts.playAudio(songName)
-            elif ('para' in action or 'quita' in action) and 'cancion' in action: # Stop actual song
+            elif ('para' in action or 'quita' in action) and 'canción' in action: # Stop actual song
                 activeBool = tts.stopAudio()
             elif 'pausa' in action: # Pause actual song
                 activeBool = tts.pauseAudio()
             elif 'continua' in action or 'reanuda' in action: # Resume actual song
                 activeBool = tts.resumeAudio()
             elif ('escanea' in action or 'busca' in action) and 'código' in action: # Search for Qr Code in cam
+                camCapture = True
+                camCaptureChanged.set()
+                camCapture = False
                 activeBool = tts.getQrCode()
             elif 'hasta luego' in action: # Close app
                 tts.goodbye()
-                activeModel.set() # Send an advise to the Kivy's Interface Thread to close the app
+                # Close Tkinter gui
 
 if __name__ == '__main__':
     print('+------------------------------------+')
--- MODEL/TextToSpeech_V1/main.py
+++ MODEL/TextToSpeech_V1/main.py
@@ -2,9 +2,6 @@
 import sys
 from pathlib import Path
 import pyttsx3
-import logging
-
-logging.basicConfig(level=logging.INFO, filename="TEMPFILES/mainLog.log",filemode="a")
 
 root_path = Path(__file__).resolve().parents[2]
 sys.path.append(str(root_path))
@@ -22,121 +19,110 @@ engine.setProperty('rate', rate-50)
 def soundTest():
     try:
         print('TextToSpeech || SoundTest')
-        logging.info("TextToSpeech || SoundTest")
         engine.say(lang.esp['test'])
         engine.runAndWait()
         engine.stop()
         return False
     except Exception:
-        logging.error(f"TextToSpeech || SoundTest: {Exception}")
+        print(f"TextToSpeech || SoundTest: {Exception}")
         return False
 
 def welcome():
     try:
         print('TextToSpeech || Welcome')
-        logging.info("TextToSpeech || Welcome")
         engine.say(lang.esp['welcome'])
         engine.runAndWait()
     except Exception:
-        logging.error(f"TextToSpeech || Welcome: {Exception}")
+        print(f"TextToSpeech || Welcome: {Exception}")
 
 def ActualTime():
     try:
         print('TextToSpeech || ActualTime')
-        logging.info("TextToSpeech || ActualTime")
         acTime = time.getActualTime()
         engine.say(acTime)
         engine.runAndWait()
         return False
     except Exception:
-        logging.error(f"TextToSpeech || ActualTime: {Exception}")
+        print(f"TextToSpeech || ActualTime: {Exception}")
         return False
 
 def ActualDay():
     try:
         print('TextToSpeech || ActualDay')
-        logging.info("TextToSpeech || ActualDay")
         acDay = time.getActualDay()
         engine.say(acDay)
         engine.runAndWait()
         return False
     except Exception:
-        logging.error(f"TextToSpeech || ActualDay: {Exception}")
+        print(f"TextToSpeech || ActualDay: {Exception}")
         return False
 
 def SetVolume(value):
     try:
         print(f'TextToSpeech || SetVolume ({value})')
-        logging.info(f"TextToSpeech || SetVolume ({value})")
         volumeControl.set_volume(value)
         return False
     except Exception:
-        logging.error(f"TextToSpeech || SetVolume: {Exception}")
+        print(f"TextToSpeech || SetVolume: {Exception}")
         return False
 
 def playAudio(name):
     try:
         print(f'TextToSpeech || PlayAudio ({name})')
-        logging.info(f"TextToSpeech || PlayAudio ({name})")
         engine.say(f'Reproduciendo {name}')
         engine.runAndWait()
         audioPath = soundmusic.audioDownload(name)
         soundmusic.audioPlay(audioPath)
         return False
     except Exception:
-        logging.error(f"TextToSpeech || PlayAudio: {Exception}")
+        print(f"TextToSpeech || PlayAudio: {Exception}")
         return False
 
 def stopAudio():
     try:
         print(f'TextToSpeech || StopAudio')
-        logging.info("TextToSpeech || StopAudio")
         soundmusic.audioStop()
         return False
     except Exception:
-        logging.error(f"TextToSpeech || StopAudio: {Exception}")
+        print(f"TextToSpeech || StopAudio: {Exception}")
         return False
 
 def pauseAudio():
     try:
         print(f'TextToSpeech || PauseAudio')
-        logging.info("TextToSpeech || PauseAudio")
         soundmusic.audioPause()
         return False
     except Exception:
-        logging.error(f"TextToSpeech || PauseAudio: {Exception}")
+        print(f"TextToSpeech || PauseAudio: {Exception}")
         return False
 
 def resumeAudio():
     try:
         print(f'TextToSpeech || ResumeAudio')
-        logging.info("TextToSpeech || ResumeAudio")
         soundmusic.audioResume()
         return False
     except Exception:
-        logging.error(f"TextToSpeech || ResumeAudio: {Exception}")
+        print(f"TextToSpeech || ResumeAudio: {Exception}")
         return False
 
 def getQrCode():
     try:
         print(f'TextToSpeech || GetQrCode')
-        logging.info("TextToSpeech || GetQrCode")
         engine.say("Aquí tienes el contenido del código qr")
         engine.runAndWait()
         qrcode.qrSearch()
         return False
     except Exception:
-        logging.error(f"TextToSpeech || GetQrCode: {Exception}")
+        print(f"TextToSpeech || GetQrCode: {Exception}")
         return False
 
 def goodbye():
     try:
         print('TextToSpeech || Goodbye')
-        logging.info("TextToSpeech || Goodbye")
         engine.say(lang.esp['goodbye'])
         engine.runAndWait()
     except Exception:
-        logging.error(f"TextToSpeech || Goodbye: {Exception}")
+        print(f"TextToSpeech || Goodbye: {Exception}")
         return False
 
 if __name__ == "__main__":
