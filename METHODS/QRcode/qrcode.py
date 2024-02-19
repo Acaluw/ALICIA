4d67c83f1acc6e49b9e6fe6f56e7c6067a1fb93f
Andrew <andrewapn38@gmail.com>
2024-02-19 17:34:48 +0100

    Update qrcode.py

--- METHODS/QRcode/qrcode.py
+++ METHODS/QRcode/qrcode.py
@@ -2,19 +2,19 @@
 import cv2
 import webbrowser
 
-# Función para abrir una página web en el navegador
+# Function to open a web page in the browser
 def openUrl(url):
     webbrowser.open(url)
 
 def qrSearch():
-    # Detectamos y decodificamos si hay un posible código QR en el fotograma
+    # Detect and decode if there is a possible QR code in the frame
     frame = cv2.imread('TEMPFILES/images/productImg.png')
     qrDetector = cv2.QRCodeDetector()
     data, bbox, rectifiedImage = qrDetector.detectAndDecode(frame)
-    # Si se detecta un código QR
+    # If a QR code is detected
     if len(data) > 0:
         print(f'Dato: {data}')
-        # Abrir la página web correspondiente al dato del código QR
+        # Open the corresponding web page for the QR code data
         openUrl(data)
 
 if __name__ == '__main__':
