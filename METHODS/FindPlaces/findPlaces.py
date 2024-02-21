b47442e490f0c56f50136997b8c5469a31012bd2
Andrew <andrewapn38@gmail.com>
2024-02-21 19:33:17 +0100

    Update findPlaces.py

--- METHODS/FindPlaces/findPlaces.py
+++ METHODS/FindPlaces/findPlaces.py
@@ -1,11 +1,11 @@
 import requests
 import geocoder
+from geopy.geocoders import Nominatim
 
-
-def buscar_restaurantes_cercanos(latitud, longitud, tipo="bar", radio = 10000, limite=10):
+def buscar_restaurantes_cercanos(latitud, longitud, tipo = "", radio = 10000, limite=10):
+    tipo = busqueda_tipo(frase)
     api_key = "AIzaSyCpJ8LYkRE8KYw6GKJ1p4Q_CZH0Ct95iHA"
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitud},{longitud}&rankby=distance&type={tipo}&key={api_key}"
-    # url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitud},{longitud}&rankby=distance&radius={radio}&type={tipo}&key={api_key}"
     response = requests.get(url)
     datos = response.json()
     
@@ -18,7 +18,43 @@ def buscar_restaurantes_cercanos(latitud, longitud, tipo="bar", radio = 10000, l
     else:
         print("No se encontraron restaurantes cercanos.")
 
-from geopy.geocoders import Nominatim
+def busqueda_tipo(frase):
+    tipos_disponibles = {
+        "restaurante": "restaurant",
+        "cafe": "cafe",
+        "bar": "bar",
+        "panaderia": "bakery",
+        "supermercado": "grocery_or_supermarket",
+        "hospital": "hospital",
+        "farmacia": "pharmacy",
+        "banco": "bank",
+        "cajero": "atm",
+        "parque": "park",
+        "gimnasio": "gym",
+        "cine": "movie_theater",
+        "teatro": "movie_theater",
+        "centro comercial": "shopping_mall",
+        "centros comerciales": "shopping_mall",
+        "libreria": "book_store",
+        "gasolinera": "gas_station",
+        "estacion de tren": "train_station",
+        "estaciones de trenes": "train_station",
+        "aeropuerto": "airport",
+        "estacion de autobus": "bus_station",
+        "estaciones de autobuses": "bus_station",
+        "parada de autobus": "bus_station",
+        "paradas de autobus": "bus_station",
+        "parada de taxi": "taxi_stand",
+        "paradas de taxi": "taxi_stand",
+        "alojamiento": "lodging",
+        "hotel": "lodging"
+    }
+    
+    for palabra, tipo in tipos_disponibles.items():
+        if palabra in frase.lower():
+            return tipo
+    
+    return None
 
 def obtener_latitud_longitud():
     try:
@@ -44,4 +80,5 @@ latitud = 37.19091096796008
 longitud = -3.6300729939327936
 # latitud = 37.16062868658626
 # longitud = -3.5965281246050145
+frase = input("Introduce la frase: ")
 buscar_restaurantes_cercanos(latitud, longitud)
